d810c98e9d1ae8cf4277e06ac284e6ce54599c26
wsavich <warrensavich@gmail.com>
2013-01-19 23:04:05 -0600

    fix security hole

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
 
-  #before_filter :require_login
+  before_filter :require_login
 
   private
 
